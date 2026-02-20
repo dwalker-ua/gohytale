d4e254f49215ea842603786506282b39a63564e1
dwalker-ua <144023434+dwalker-ua@users.noreply.github.com>
2026-02-20 11:54:44 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 let serverIP = "play.gohytale.net:15662";
+let server_state = false;
+let server_players = 0;
 
 let address_field = document.getElementById("address_field");
 address_field.value = serverIP;
+
+let server_status = document.getElementById("server_status");
 
 function copyAddress() {
     navigator.clipboard.writeText(serverIP);
@@ -10,4 +14,26 @@ function copyAddress() {
     setTimeout(() => { address_field.value = serverIP; }, 3000);
 }
 
+async function updateServerMetric() {
+    const workerUrl = "https://gohytale-server-status.arthur2rock-live.workers.dev";
+
+    try {
+        const response = await fetch(workerUrl);
+        const data = await response.json();
+
+        server_state = data.online;
+        server_players = data.players;
+
+        if (server_state) {
+            if (server_players > 0)
+                server_status.innerHTML = `${server_players} online`;
+        } else {
+            server_status.innerHTML = "offline";
+            server_status.classList.add("offline");
+        }
+    }
+    catch {}
+}
+
 address_field.addEventListener("click", copyAddress, false);
+updateServerMetric();
